635cbe73aa27d810bec17175a393210837a58c26
lord-charles <mwanikicharles226@gmail.com>
2023-10-19 12:26:47 +0300

    first commit

---
+++ app/components/scroll-to-top/index.js
@@ -0,0 +1,56 @@
+"use client"
+
+// ** MUI Imports
+import React from 'react';
+import { Zoom, styled } from '@mui/material';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Fab from '@mui/material/Fab'
+import ArrowUp from 'mdi-material-ui/ArrowUp'
+import Image from 'next/image';
+
+
+const ScrollToTopStyled = styled('div')(({ theme }) => ({
+  zIndex: 11,
+  position: 'fixed',
+  right: theme.spacing(6),
+  bottom: theme.spacing(10)
+}))
+
+
+
+
+
+const ScrollToTop = () => {
+
+  // ** init trigger
+  const trigger = useScrollTrigger({
+    threshold: 400,
+    disableHysteresis: true
+  })
+
+  const handleClick = () => {
+    const anchor = document.querySelector('body')
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+
+
+  return (
+    <>
+    <Zoom in={trigger} onClick={handleClick}>
+    <ScrollToTopStyled>
+    <Fab color='success' size='small' aria-label='scroll back to top' className="bg-purple-500">
+            <ArrowUp />
+          </Fab>
+    </ScrollToTopStyled>
+  </Zoom>
+        
+       </>
+         
+
+  )
+}
+
+export default ScrollToTop
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import { Footer } from "./components";
-
+import ScrollToTop from "./components/scroll-to-top/index";
 
 export const metadata = {
   title: "Rift Homes",
@@ -12,13 +12,15 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="w-screen overflow-x-hidden">
         <Navbar />
         {children}
+        <ScrollToTop />
+
         <Footer />
       </body>
     </html>
